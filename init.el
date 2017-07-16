2e5f9a77f3907b44464f6c0871154e9b524874d2
Eric James Michael Ritz <ericjmritz@yandex.com>
2017-07-16 06:16:31 -0400

    [Feature] Multiple Cursors
    This patch installs Multiple Cursors and binds `C-s-c` to a Hydra for
    various multpile-cursor commands, which is from Hydra's GitHub wiki.

--- init.el
+++ init.el
@@ -121,6 +121,29 @@
   (use-package ivy-historian
     :config (ivy-historian-mode t)))
 
+(use-package multiple-cursors
+  :config
+  (defhydra hydra-multiple-cursors (:hint nil)
+    "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+    ("l" mc/edit-lines :exit t)
+    ("a" mc/mark-all-like-this :exit t)
+    ("n" mc/mark-next-like-this)
+    ("N" mc/skip-to-next-like-this)
+    ("M-n" mc/unmark-next-like-this)
+    ("p" mc/mark-previous-like-this)
+    ("P" mc/skip-to-previous-like-this)
+    ("M-p" mc/unmark-previous-like-this)
+    ("r" mc/mark-all-in-region-regexp :exit t)
+    ("q" nil))
+  (bind-key "C-s-c" #'hydra-multiple-cursors/body))
+
 (use-package anyins
   :commands (anyins-mode)
   :bind ("C-x r a" . anyins-mode))
